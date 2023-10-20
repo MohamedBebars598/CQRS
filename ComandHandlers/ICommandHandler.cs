aac6856377469e5e64f6fdf0373aba9c12487dae
MohamedBebars598 <mohamedbibers2556@gmail.com>
2023-10-20 16:26:31 +0300

    organize the library and create command and command handler blueprint

---
+++ ComandHandlers/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSBebars.ComandHandlers
+{
+    public interface ICommandHandler<TCommand,TResult>
+    {
+        public Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+
+
+    }
+}
