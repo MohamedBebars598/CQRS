da1b0c54c9482e68cd60e7210085cb27aeb5ef13
MohamedBebars <mohamedbibers2556@gmail.com>
2023-10-24 23:11:51 +0300

    remove unnessary using namespaces

--- ComandHandlers/ICommandHandler.cs
+++ ComandHandlers/ICommandHandler.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CQRSBebars.Comand;
 
 namespace CQRSBebars.ComandHandlers
 {
-    public interface ICommandHandler<TCommand,TResult>
+    public interface ICommandHandler<TCommand,TResult> where TCommand : class, ICommand
     {
         public Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
 
