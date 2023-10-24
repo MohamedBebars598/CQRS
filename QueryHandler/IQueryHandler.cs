db27b58ae4470453497e629fe10a47569c65a4ba
MohamedBebars <mohamedbibers2556@gmail.com>
2023-10-24 23:13:38 +0300

    create query handler blueprint

---
+++ QueryHandler/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using CQRSBebars.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSBebars.QueryHandler
+{
+    public interface IQueryHandler<in TQuery, TResult> where TQuery : class, IQuery<TResult>
+    {
+        Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
+    }
+}
