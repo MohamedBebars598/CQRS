3297e263ba2b08f5f82d413b4f8bb61df9cd8f86
MohamedBebars <mohamedbibers2556@gmail.com>
2023-10-24 23:16:15 +0300

    create Query sender blueprint

---
+++ QuerySender/IQuerySender.cs
@@ -0,0 +1,14 @@
+﻿using CQRSBebars.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSBebars.QuerySender
+{
+    public interface IQuerySender
+    {
+        Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default);
+    }
+}
